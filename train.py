d1b4e9870c9822eb3c5c40b73e4c506f8775af55
Vadim Kantorov <vadimkantorov@gmail.com>
2019-06-24 15:31:21 +0500

    ...

--- train.py
+++ train.py
@@ -1,17 +1,22 @@
+import torch
+import gc
+import time
 import argparse
 import torch
 import torch.utils.data
+#import torch.utils.tensorboard
 
-import data
 import data.dataset
+import model
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--train-data-path', default = '../open_stt_splits/audiobooks_train.csv.gz')
+parser.add_argument('--val-data-path', default = '../open_stt_splits/audiobooks_val.csv.gz')
 parser.add_argument('--sample-rate', type = int, default = 16000)
 parser.add_argument('--window-size', type = float, default = 0.2)
 parser.add_argument('--window-stride', type = float, default = 0.1)
 parser.add_argument('--window', default = 'hann', choices = ['hann', 'hamming'])
-parser.add_argument('--num-workers', type = int, default = 10)
+parser.add_argument('--num-workers', type = int, default = 1)
 args = parser.parse_args()
 
 audio_conf = dict(
@@ -21,9 +26,15 @@ audio_conf = dict(
 	window = args.window
 )
 
-train_dataset = data.dataset.SpectrogramDataset(audio_conf = audio_conf, data_path = args.train_data_path)
-train_loader = torch.utils.data.DataLoader(train_dataset, num_workers = args.num_workers, collate_fn = data.dataset.collate_fn, pin_memory = True, shuffle = True)
+if __name__ == '__main__':
+    torch.multiprocessing.set_start_method('spawn', force = True)
 
-for i, (inputs, targets, filenames, input_percentages, target_sizes) in enumerate(train_loader):
-    print(i, inputs.shape)
-    break
+    train_dataset = data.dataset.SpectrogramDataset(audio_conf = audio_conf, data_path = args.train_data_path)
+    train_loader = torch.utils.data.DataLoader(train_dataset, num_workers = args.num_workers, collate_fn = data.dataset.collate_fn, pin_memory = True, shuffle = True)
+
+    val_dataset = data.dataset.SpectrogramDataset(audio_conf = audio_conf, data_path = args.val_data_path)
+    val_loader = torch.utils.data.DataLoader(val_dataset, num_workers = args.num_workers, collate_fn = data.dataset.collate_fn, pin_memory = True, shuffle = False)
+
+    for i, (inputs, targets, filenames, input_percentages, target_sizes) in enumerate(train_loader):
+        print(i, inputs.shape)
+        break
