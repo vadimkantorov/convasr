8288e6679f868d18e2a4489cfef5f98901b72fd3
Vadim Kantorov <vadimkantorov@gmail.com>
2019-07-01 12:50:14 +0500

    ...

--- train.py
+++ train.py
@@ -28,62 +28,67 @@ parser.add_argument('--device', default = 'cuda', choices = ['cuda', 'cpu'])
 parser.add_argument('--checkpoint')
 parser.add_argument('--checkpoint-dir', default = 'data/checkpoints')
 parser.add_argument('--model', default = 'Wav2LetterVanilla')
+parser.add_argument('--log-dir')
+parser.add_argument('--max-norm', type = float, default = 0.0)
 args = parser.parse_args()
 
-if __name__ == '__main__':
-    #torch.multiprocessing.set_start_method('spawn', force = True) # https://github.com/pytorch/pytorch/issues/22131
-    labels = data.labels.RU_LABELS
-    num_classes = len(labels)
+tb = torch.utils.tensorboard.SummaryWriter(args.log_dir)
 
-    train_dataset = data.dataset.SpectrogramDataset(sample_rate = args.sample_rate, window_size = args.window_size, window_stride = args.window_stride, window = args.window, data_path = args.train_data_path, labels = labels)
-    train_sampler = data.dataset.BucketingSampler(train_dataset, batch_size=args.train_batch_size)
-    train_loader = torch.utils.data.DataLoader(train_dataset, num_workers = args.num_workers, collate_fn = data.dataset.collate_fn, pin_memory = True, batch_sampler = train_sampler)
+#torch.multiprocessing.set_start_method('spawn', force = True) # https://github.com/pytorch/pytorch/issues/22131
+labels = data.labels.RU_LABELS
+num_classes = len(labels)
 
-    val_dataset = data.dataset.SpectrogramDataset(sample_rate = args.sample_rate, window_size = args.window_size, window_stride = args.window_stride, window = args.window, data_path = args.val_data_path, labels = labels)
-    val_loader = torch.utils.data.DataLoader(val_dataset, num_workers = args.num_workers, collate_fn = data.dataset.collate_fn, pin_memory = True, shuffle = False, batch_size = args.val_batch_size)
+train_dataset = data.dataset.SpectrogramDataset(sample_rate = args.sample_rate, window_size = args.window_size, window_stride = args.window_stride, window = args.window, data_path = args.train_data_path, labels = labels)
+train_sampler = data.dataset.BucketingSampler(train_dataset, batch_size=args.train_batch_size)
+train_loader = torch.utils.data.DataLoader(train_dataset, num_workers = args.num_workers, collate_fn = data.dataset.collate_fn, pin_memory = True, batch_sampler = train_sampler)
 
-    device = torch.device(args.device)
+val_dataset = data.dataset.SpectrogramDataset(sample_rate = args.sample_rate, window_size = args.window_size, window_stride = args.window_stride, window = args.window, data_path = args.val_data_path, labels = labels)
+val_loader = torch.utils.data.DataLoader(val_dataset, num_workers = args.num_workers, collate_fn = data.dataset.collate_fn, pin_memory = True, shuffle = False, batch_size = args.val_batch_size)
 
-    model = model.Speech2TextModel(getattr(model, args.model)(num_classes))
-    if args.checkpoint:
-        model.load_checkpoint(args.checkpoint)
+device = torch.device(args.device)
 
-    model = model.to(device)
-    optimizer = torch.optim.SGD(model.parameters(), lr = args.lr, momentum = args.momentum, weight_decay = args.weight_decay)
-    criterion = CTCLoss()
-    decoder = decoder.GreedyDecoder(labels)
-    
-    print('Dataset loaded')
-    for epoch in range(args.epochs):
-        for i, (inputs, targets, filenames, input_percentages, target_sizes) in enumerate(train_loader):
-            input_sizes = input_percentages.mul_(int(inputs.size(3))).int()
-            logits, probs, output_sizes = model(inputs.to(device), input_sizes.to(device))
+model = model.Speech2TextModel(getattr(model, args.model)(num_classes))
+if args.checkpoint:
+    model.load_checkpoint(args.checkpoint)
 
-            loss = criterion(logits.transpose(0, 1), targets, output_sizes.cpu(), target_sizes) # logits -> TxNxH
-            loss = loss / len(inputs) 
-            loss = loss.to(device)
+model = model.to(device)
+optimizer = torch.optim.SGD(model.parameters(), lr = args.lr, momentum = args.momentum, weight_decay = args.weight_decay)
+criterion = CTCLoss()
+decoder = decoder.GreedyDecoder(labels)
 
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
-            print('Iteration', i, 'loss:', float(loss))
+print('Dataset loaded')
+for epoch in range(args.epochs):
+    for i, (inputs, targets, filenames, input_percentages, target_sizes) in enumerate(train_loader):
+        input_sizes = input_percentages.mul_(int(inputs.size(3))).int()
+        logits, probs, output_sizes = model(inputs.to(device), input_sizes.to(device))
 
-        num_words, num_chars, val_wer_sum, val_cer_sum = 0, 0, 0.0, 0.0
+        loss = criterion(logits.transpose(0, 1), targets, output_sizes.cpu(), target_sizes) # logits -> TxNxH
+        loss = loss / len(inputs) 
+        loss = loss.to(device)
 
-        for i, (inputs, targets, filenames, input_percentages, target_sizes) in enumerate(train_loader):
-            input_sizes = input_percentages.mul_(int(inputs.size(3))).int()
-            logits, probs, output_sizes = model(inputs.to(device), input_sizes.to(device))
-            decoded_output, _ = decoder.decode(probs, output_sizes)
-            target_strings = decoder.convert_to_strings(data.dataset.unpack_targets(targets, target_sizes))
-            for x in range(len(target_strings)):
-                transcript, reference = decoded_output[x][0], target_strings[x][0]
-                wer, cer, wer_ref, cer_ref = data.dataset.get_cer_wer(decoder, transcript, reference)
-                val_wer_sum += wer
-                val_cer_sum += cer
-                num_words += wer_ref
-                num_chars += cer_ref
-        print('WER: {:.02%} CER: {:.02%}'.format(val_wer_sum / num_words, val_cer_sum / num_chars))
+        optimizer.zero_grad()
+        loss.backward()
+        if args.max_norm > 0:
+            torch.nn.utils.clip_grad_norm_(model.parameters(), args.max_norm)
+        optimizer.step()
+        print('Iteration', i, 'loss:', float(loss))
 
-        if not os.path.exists(checkpoint_dir):
-            os.makedirs(checkpoint_dir)
-        model.save_checkpoint(arg.checkpoint_dir)
+    num_words, num_chars, val_wer_sum, val_cer_sum = 0, 0, 0.0, 0.0
+
+    for i, (inputs, targets, filenames, input_percentages, target_sizes) in enumerate(train_loader):
+        input_sizes = input_percentages.mul_(int(inputs.size(3))).int()
+        logits, probs, output_sizes = model(inputs.to(device), input_sizes.to(device))
+        decoded_output, _ = decoder.decode(probs, output_sizes)
+        target_strings = decoder.convert_to_strings(data.dataset.unpack_targets(targets, target_sizes))
+        for x in range(len(target_strings)):
+            transcript, reference = decoded_output[x][0], target_strings[x][0]
+            wer, cer, wer_ref, cer_ref = data.dataset.get_cer_wer(decoder, transcript, reference)
+            val_wer_sum += wer
+            val_cer_sum += cer
+            num_words += wer_ref
+            num_chars += cer_ref
+    print('WER: {:.02%} CER: {:.02%}'.format(val_wer_sum / num_words, val_cer_sum / num_chars))
+
+    if not os.path.exists(checkpoint_dir):
+        os.makedirs(checkpoint_dir)
+    model.save_checkpoint(arg.checkpoint_dir)
