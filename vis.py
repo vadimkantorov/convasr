9ee2267cf3917230f73cc7ee7f21a02d9e7b2fc2
Vadim Kantorov <vadimkantorov@gmail.com>
2019-07-25 13:14:41 +0000

    ...

--- vis.py
+++ vis.py
@@ -15,6 +15,6 @@ vis.write(f'<html><meta charset="utf-8"><body><h1>{args.transcripts}</h1><table>
 
 for i, (reference, transcript, filename, cer) in enumerate(list(map(j.get, ['reference', 'transcript', 'filename', 'cer'])) for j in ref_tra):
     encoded = base64.b64encode(open(filename, 'rb').read()).decode('utf-8').replace('\n', '')
-    vis.write(f'<tr><td style="border-right: 2px black solid">{cer:.02f}</td> <td style="font-size:xx-small">{os.path.basename(filename)}</td> <td><audio controls src="data:audio/wav;base64,{encoded}"/></td><td><b>{reference}</b></td><td>{transcript}</td></tr>\n')
+    vis.write(f'<tr><td style="border-right: 2px black solid">{cer:.02f}</td> <td style="font-size:xx-small">{os.path.basename(filename)}</td> <td><audio controls src="data:audio/wav;base64,{encoded}"/></td><td><div><b>{reference}</b></div><div>{transcript}</div></td></tr>\n')
 
 vis.write('</table></body></html>') 
