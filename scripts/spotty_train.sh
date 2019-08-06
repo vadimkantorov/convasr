5e364d6ad425c351e3399830696073d85dd4c7ea
Vadim Kantorov <vadimkantorov@gmail.com>
2019-08-06 15:02:03 +0500

    ...

--- scripts/spotty_train.sh
+++ scripts/spotty_train.sh
@@ -1,11 +1,11 @@
 spotty run -c scripts/spotty.yaml train \
- -p CUDA_VISIBLE_DEVICES=0,1 \
+ -p CUDA_VISIBLE_DEVICES=0,1,2,3 \
  -p ARGS="--verbose --lang ru \
   --model Wav2LetterRu \
   --train-batch-size 40 --val-batch-size 40 \
-  --lr 1e-2 --optimizer AdamW \
+  --lr 1e-2 --optimizer SGD \
   --train-data-path /root/convasr/data/clean_train.csv \
-  --val-batch-period 5000 \
   --val-data-path /root/convasr/data/clean_val.csv \
+  --val-iteration-interval 100 \
   --epochs 1 
 "
