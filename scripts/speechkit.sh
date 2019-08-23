c98011bb1e9d39d04817c59e3d406a388530d0d3
Vadim Kantorov <vadimkantorov@gmail.com>
2019-08-23 09:16:14 +0000

    ...

---
+++ scripts/speechkit.sh
@@ -0,0 +1,29 @@
+INPUTFILE=$1
+export OUTPUTDIR=$2
+NJOBS=${3:-20}
+if [ -z $SPEECHKITAPIKEY ]; then
+	SPEECHKITAPIKEY=$(cat speechkitapikey.txt)
+fi
+
+random_voice() {
+	P=(alyss jane oksana omazh zahar ermil)
+	echo ${P[RANDOM%${#P[@]}]}
+}
+random_emotion() {
+	P=(good evil neutral)
+	echo ${P[RANDOM%${#P[@]}]}
+}
+random_speed() {
+	P=(0.7 0.8 1.0)
+	echo ${P[RANDOM%${#P[@]}]}
+}
+
+export -f random_voice
+export -f random_emotion
+export -f random_speed
+
+mkdir -p "$OUTPUTDIR"
+cat "$INPUTFILE" | parallel -j$NJOBS 'curl -s -X POST -H "Authorization: Api-Key $SPEECHKITAPIKEY"  -d "lang=ru-RU&voice=$(random_voice)&emotion=$(random_emotion)&speed=$(random_speed)" https://tts.api.cloud.yandex.net/speech/v1/tts:synthesize --data-urlencode text={} > "$OUTPUTDIR/$(echo -n {} | md5sum | cut -d" " -f1).ogg"'
+
+
+
