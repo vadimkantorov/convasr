48fd57e3c15c5b89b3e8a55e23577820d00dfb67
Vadim Kantorov <vadimkantorov@gmail.com>
2019-09-17 14:46:08 +0000

    ...

--- models.py
+++ models.py
@@ -7,9 +7,9 @@ import torch.nn.functional as F
 import librosa
 
 class ConvBNReLUDropout(nn.Sequential):
-	def __init__(self, kernel_size, num_channels, stride = 1, dropout = 0, batch_norm_momentum = 0.1, residual = True):
+	def __init__(self, kernel_size, num_channels, stride = 1, dropout = 0, batch_norm_momentum = 0.1, residual = True, groups = 1):
 		super().__init__(collections.OrderedDict(zip(['0', '1', '2'], [ #['conv', 'bn', 'relu_dropout'], [
-			nn.Conv1d(num_channels[0], num_channels[1], kernel_size = kernel_size, stride = stride, padding = max(1, kernel_size // 2), bias = False),
+			nn.Conv1d(num_channels[0], num_channels[1], kernel_size = kernel_size, stride = stride, padding = max(1, kernel_size // 2), bias = False, groups = groups),
 			nn.BatchNorm1d(num_channels[1], momentum = batch_norm_momentum),
 			ReLUDropout(p = dropout, inplace = True)
 			]))
@@ -21,6 +21,14 @@ class ConvBNReLUDropout(nn.Sequential):
 		if not self.residual or self[0].in_channels != self[0].out_channels or y.shape[-1] != x.shape[-1]:
 			return y
 		return y + x
+
+class ConvBNClip(nn.Sequential):
+	def __init__(self, kernel_size, num_channels, stride = 1, dilation = 1, repeat = 1, padding = 0, max_value = 20):
+		modules = []
+		for i in range(repeat):
+			modules.append(nn.Conv1d(num_channels[0] if i == 0 else num_channels[1], num_channels[1], kernel_size = kernel_size, stride = stride, dilation = dilation, padding = padding))
+			modules.append(nn.Hardtanh(0, max_value, inplace = True))
+		super().__init__(*modules)
 
 class InvertedResidual(nn.Module):
 	def __init__(self, kernel_size, num_channels, stride = 1, dilation = 1, dropout = 0.2, expansion = 1, squeeze_excitation_ratio = 0.25, batch_norm_momentum = 0.1, separable = True, simple = False, residual = True):
@@ -75,29 +83,29 @@ class InvertedResidual(nn.Module):
 
 class BabbleNet(nn.Sequential):
 	def __init__(self, num_classes, num_input_features, dropout = 0.2, repeat = 1, batch_norm_momentum = 0.1):
-			super().__init__(
-				ConvBNReLUDropout(kernel_size = 13, num_channels = (num_input_features, 768), stride = 2, dropout = dropout),
+		super().__init__(
+			ConvBNReLUDropout(kernel_size = 13, num_channels = (num_input_features, 768), stride = 2, dropout = dropout),
 
-				ConvBNReLUDropout(kernel_size = 13, num_channels = (768, 192), stride = 1, dropout = dropout),
-				InvertedResidual(kernel_size = 13, num_channels = (192, 192), stride = 1, dropout = dropout, expansion = 4),
-				InvertedResidual(kernel_size = 13, num_channels = (192, 192), stride = 1, dropout = dropout, expansion = 4),
-				InvertedResidual(kernel_size = 13, num_channels = (192, 192), stride = 1, dropout = dropout, expansion = 4),
-				InvertedResidual(kernel_size = 13, num_channels = (192, 192), stride = 1, dropout = dropout, expansion = 4),
-				InvertedResidual(kernel_size = 13, num_channels = (192, 192), stride = 1, dropout = dropout, expansion = 4),
-				ConvBNReLUDropout(kernel_size = 13, num_channels = (192, 768), stride = 1, dropout = dropout),
+			ConvBNReLUDropout(kernel_size = 13, num_channels = (768, 192), stride = 1, dropout = dropout),
+			InvertedResidual(kernel_size = 13, num_channels = (192, 192), stride = 1, dropout = dropout, expansion = 4),
+			InvertedResidual(kernel_size = 13, num_channels = (192, 192), stride = 1, dropout = dropout, expansion = 4),
+			InvertedResidual(kernel_size = 13, num_channels = (192, 192), stride = 1, dropout = dropout, expansion = 4),
+			InvertedResidual(kernel_size = 13, num_channels = (192, 192), stride = 1, dropout = dropout, expansion = 4),
+			InvertedResidual(kernel_size = 13, num_channels = (192, 192), stride = 1, dropout = dropout, expansion = 4),
+			ConvBNReLUDropout(kernel_size = 13, num_channels = (192, 768), stride = 1, dropout = dropout),
 
-				ConvBNReLUDropout(kernel_size = 31, num_channels = (768, 2048), stride = 1, dropout = dropout),
-				ConvBNReLUDropout(kernel_size = 1,  num_channels = (2048, 2048), stride = 1, dropout = dropout),
-				nn.Conv1d(2048, num_classes, kernel_size = 1)
-			)
+			ConvBNReLUDropout(kernel_size = 31, num_channels = (768, 2048), stride = 1, dropout = dropout),
+			ConvBNReLUDropout(kernel_size = 1,  num_channels = (2048, 2048), stride = 1, dropout = dropout),
+			nn.Conv1d(2048, num_classes, kernel_size = 1)
+		)
 
 	def forward(self, x, input_lengths):
 		return super().forward(x)
 
 #TODO: apply conv masking
 class Wav2LetterRu(nn.Sequential):
-	def __init__(self, num_classes, num_input_features, dropout = 0.2):
-		layers = [
+	def __init__(self, num_classes, num_input_features, dropout = 0.2, width_large = 2048, kernel_size_large = 31):
+		super().__init__(
 			ConvBNReLUDropout(kernel_size = 13, num_channels = (num_input_features, 768), stride = 2, dropout = dropout),
 			
 			ConvBNReLUDropout(kernel_size = 13, num_channels = (768, 768), stride = 1, dropout = dropout),
@@ -107,40 +115,33 @@ class Wav2LetterRu(nn.Sequential):
 			ConvBNReLUDropout(kernel_size = 13, num_channels = (768, 768), stride = 1, dropout = dropout),
 			ConvBNReLUDropout(kernel_size = 13, num_channels = (768, 768), stride = 1, dropout = dropout),
 
-			ConvBNReLUDropout(kernel_size = 31, num_channels = (768, 2048), stride = 1, dropout = dropout),
-			ConvBNReLUDropout(kernel_size = 1,  num_channels = (2048, 2048),stride = 1, dropout = dropout),
-			nn.Conv1d(2048, num_classes, kernel_size = 1, stride = 1)
-		]
-		super().__init__(*layers)
+			ConvBNReLUDropout(kernel_size = kernel_size_large, num_channels = (768, width_large), stride = 1, dropout = dropout, groups = 16),
+			ConvBNReLUDropout(kernel_size = 1,  num_channels = (width_large, width_large),stride = 1, dropout = dropout),
+			nn.Conv1d(width_large, num_classes, kernel_size = 1, stride = 1)
+		)
 
 	def forward(self, x, input_lengths):
 		return super().forward(x)
 
 class Wav2LetterVanilla(nn.Sequential):
-	def __init__(self, num_classes, num_input_features):
-		def conv_bn_clip(kernel_size, num_channels, stride = 1, dilation = 1, repeat = 1, padding = 0):
-			modules = []
-			for i in range(repeat):
-				modules.append(nn.Conv1d(num_channels[0] if i == 0 else num_channels[1], num_channels[1], kernel_size = kernel_size, stride = stride, dilation = dilation, padding = padding))
-				modules.append(nn.Hardtanh(0, 20, inplace = True))
-			return nn.Sequential(*modules)
-
-		layers = [
-			conv_bn_clip(kernel_size = 11, num_channels = (num_input_features, 256), stride = 2, padding = 5), # 64
-			conv_bn_clip(kernel_size = 11, num_channels = (256, 256), repeat = 3, padding = 5),
-			conv_bn_clip(kernel_size = 13, num_channels = (256, 384), repeat = 3, padding = 6),
-			conv_bn_clip(kernel_size = 17, num_channels = (384, 512), repeat = 3, padding = 8),
-			conv_bn_clip(kernel_size = 21, num_channels = (512, 640), repeat = 3, padding = 10),
-			conv_bn_clip(kernel_size = 25, num_channels = (640, 768), repeat = 3, padding = 12),
-			conv_bn_clip(kernel_size = 29, num_channels = (768, 896), repeat = 1, padding = 28, dilation = 2),
-			conv_bn_clip(kernel_size = 1, num_channels = (896, 1024), repeat = 1),
+	def __init__(self, num_classes, num_input_features, dilation = 2):
+		super().__init__(
+			ConvBNClip(kernel_size = 11, num_channels = (num_input_features, 256), stride = 2, padding = 5),
+			ConvBNClip(kernel_size = 11, num_channels = (256, 256), repeat = 3, padding = 5),
+			ConvBNClip(kernel_size = 13, num_channels = (256, 384), repeat = 3, padding = 6),
+			ConvBNClip(kernel_size = 17, num_channels = (384, 512), repeat = 3, padding = 8),
+			ConvBNClip(kernel_size = 21, num_channels = (512, 640), repeat = 3, padding = 10),
+			ConvBNClip(kernel_size = 25, num_channels = (640, 768), repeat = 3, padding = 12),
+			ConvBNClip(kernel_size = 29, num_channels = (768, 896), repeat = 1, padding = 28, dilation = dilation),
+			ConvBNClip(kernel_size = 1, num_channels = (896, 1024), repeat = 1),
 			nn.Conv1d(1024, num_classes, kernel_size = 1)
-		]
+		)
 
-		super().__init__(*layers)
+	def forward(self, x, input_lengths):
+		return super().forward(x)
 
 class JasperNet(nn.ModuleList):
-	def __init__(self, num_classes, num_input_features, repeat = 3, num_subblocks = 1):
+	def __init__(self, num_classes, num_input_features, repeat = 3, num_subblocks = 1, dilation = 2):
 		def conv_bn_relu_dropout_residual(kernel_size, num_channels, dropout = 0, stride = 1, dilation = 1, padding = 0, batch_norm_momentum = 0.1, repeat = 1, num_channels_residual = []):
 				return nn.ModuleDict(dict(
 					relu_dropout = ReLUDropout(p = dropout, inplace = True),
@@ -150,7 +151,7 @@ class JasperNet(nn.ModuleList):
 					bn_residual = nn.ModuleList([nn.BatchNorm1d(num_channels[1], momentum = batch_norm_momentum) for in_channels in num_channels_residual])
 				))
 
-		PROLOgue = [conv_bn_relu_dropout_residual(kernel_size = 11, num_channels = (num_input_features, 256), dropout = 0.2, padding = 5, stride = 2)]
+		prologue = [conv_bn_relu_dropout_residual(kernel_size = 11, num_channels = (num_input_features, 256), dropout = 0.2, padding = 5, stride = 2)]
 		backbone = [
 				conv_bn_relu_dropout_residual(kernel_size = 11, num_channels = (256, 256), dropout = 0.2, padding = 5, repeat = repeat, num_channels_residual = [256]),
 				conv_bn_relu_dropout_residual(kernel_size = 11, num_channels = (256, 256), dropout = 0.2, padding = 5, repeat = repeat, num_channels_residual = [256, 256]),
@@ -170,14 +171,14 @@ class JasperNet(nn.ModuleList):
 				conv_bn_relu_dropout_residual(kernel_size = 25, num_channels = (640, 768), dropout = 0.3, padding = 12, repeat = repeat, num_channels_residual = [256, 256, 384, 512, 640])
 			]
 		epilogue = [
-			conv_bn_relu_dropout_residual(kernel_size = 29, num_channels = (768, 896), dropout = 0.4, padding = 28, dilation = 2),
+			conv_bn_relu_dropout_residual(kernel_size = 29, num_channels = (768, 896), dropout = 0.4, padding = 28, dilation = dilation),
 			conv_bn_relu_dropout_residual(kernel_size = 1, num_channels = (896, 1024), dropout = 0.4),
 
 			nn.Conv1d(1024, num_classes, kernel_size = 1)
 		]
 		super().__init__(prologue + backbone + epilogue)
 
-	def forward(self, x):
+	def forward(self, x, input_lengths):
 		residual = []
 		for i, block in enumerate(list(self)[:-1]):
 			for conv, bn in zip(block.conv[:-1], block.bn[:-1]):
