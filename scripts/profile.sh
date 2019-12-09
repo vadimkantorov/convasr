c22893a7275106c987a43345123e3c07902a77c7
Vadim Kantorov <vadimkantorov@gmail.com>
2019-12-09 14:44:59 +0000

    ...

--- scripts/profile.sh
+++ scripts/profile.sh
@@ -1,1 +1,9 @@
-nvprof --print-gpu-trace python benchmark.py
+if [ ! -z "$1" ]; then
+	TRACEFILE="-o $1"
+fi
+
+#TODO: set up hw counters https://github.com/NVIDIA/apex/tree/master/apex/pyprof#hardware-counters
+
+#TODO: use https://github.com/NVIDIA/apex/tree/master/apex/pyprof
+
+nvprof --print-gpu-trace $TRACEFILE python benchmark.py
