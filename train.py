22639157164b0dc4d7316ccb97e847c122474ff2
II245 <II245>
2020-01-08 16:03:27 +0300

    fix batch idx

--- train.py
+++ train.py
@@ -227,7 +227,7 @@ def main(args):
 	
 	for epoch in range(sampler.epoch, args.epochs):
 		time_epoch_start = time.time()
-		for dataset_name_, audio_path_, reference_, x, xlen, y, ylen in train_data_loader:
+		for batch_idx, (dataset_name_, audio_path_, reference_, x, xlen, y, ylen) in enumerate(train_data_loader, start=sampler.batch_idx):		
 			toc_data = time.time()
 			lr = scheduler.get_last_lr()[0]; lr_avg = moving_avg(lr_avg, lr, max = 1)
 			
@@ -256,7 +256,7 @@ def main(args):
 			ms = lambda sec: sec * 1000
 			time_ms_data, time_ms_fwd, time_ms_bwd, time_ms_model = ms(toc_data - tic), ms(toc_fwd - toc_data), ms(toc_bwd - toc_fwd), ms(toc_bwd - toc_data)	
 			time_ms_avg = moving_avg(time_ms_avg, time_ms_data + time_ms_model, max = 10_000)
-			print(f'{args.experiment_id} | epoch: {epoch:02d} iter: [{sampler.batch_idx: >6d} / {len(train_data_loader)} {iteration: >6d}] ent: <{entropy_avg:.2f}> loss: {loss_cur:.2f} <{loss_avg:.2f}> time: ({"x".join(map(str, x.shape))}) {time_ms_data:.2f}+{time_ms_fwd:4.0f}+{time_ms_bwd:4.0f} <{time_ms_avg:.0f}> | lr: {lr:.5f}')
+			print(f'{args.experiment_id} | epoch: {epoch:02d} iter: [{batch_idx: >6d} / {len(train_data_loader)} {iteration: >6d}] ent: <{entropy_avg:.2f}> loss: {loss_cur:.2f} <{loss_avg:.2f}> time: ({"x".join(map(str, x.shape))}) {time_ms_data:.2f}+{time_ms_fwd:4.0f}+{time_ms_bwd:4.0f} <{time_ms_avg:.0f}> | lr: {lr:.5f}')
 			iteration += 1
 
 			if iteration > 0 and iteration % args.val_iteration_interval == 0:
