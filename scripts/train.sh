dd63fb00e9df7b54121640438a3fdd3473c2b2f8
II245 <II245>
2020-01-29 22:40:47 +0300

    train script update

--- scripts/train.sh
+++ scripts/train.sh
@@ -2,16 +2,17 @@ python3 train.py $@ \
   --githttp https://github.com/vadimkantorov/convasr/commit/%h \
   --verbose --lang ru \
   --model JasperNetBig \
-  --train-batch-size 128 --val-batch-size 64 \
-  --scheduler MultiStepLR --decay-milestones 25000 75000 \
+  --train-batch-size 512 --val-batch-size 512 \
+  --scheduler MultiStepLR --decay-milestones 100000 175000 \
   --lr 1e-2 \
   --optimizer NovoGrad \
   --train-data-path data/mixed_train.csv \
+  --val-data-path data/mixed_val.csv data/clean_val.csv kontur_calls_micro/kontur_calls_micro.csv kontur_calls_micro/kontur_calls_micro.0.csv kontur_calls_micro/kontur_calls_micro.1.csv data/valset_by_rec22122019.0.csv data/valset_by_rec22122019.1.csv  data/valset_by_rec22122019.csv \
   --analyze kontur_calls_micro.csv \
   --val-iteration-interval 2500 \
-  --epochs 10 --name debug --checkpoint-skip --exphtml= #\
-
-#  --val-data-path data/mixed_val.csv data/clean_val.csv kontur_calls_micro/kontur_calls_micro.csv kontur_calls_micro/kontur_calls_micro.0.csv kontur_calls_micro/kontur_calls_micro.1.csv \
+  --fp16 O2 \
+  --experiment-name long-train \
+  --epochs 40 #\
 
 #  --finetune --checkpoint-skip
 #  --bpe data/tts_dataset_bpe_5000_word.model \
