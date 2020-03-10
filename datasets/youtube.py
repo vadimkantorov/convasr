6801c158622aa6691b1a700900aa79c06730902f
Vadim Kantorov <vadimkantorov@gmail.com>
2020-03-10 18:40:14 +0000

    ...

--- datasets/youtube.py
+++ datasets/youtube.py
@@ -5,6 +5,7 @@ import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument('--input-path', '-i', required = True)
 parser.add_argument('--output-path', '-o', required = True)
+parser.add_argument('--strip', nargs = '*', default = ['begin', 'end']]
 args = parser.parse_args()
 
 transcript = []
@@ -13,7 +14,7 @@ for info_name in os.listdir(args.input_path):
 		continue
 	
 	info_path = os.path.join(args.input_path, info_name)
-	transcript.extend(dict(audio_path = info_path.replace('.json', ''), **t) for t in json.load(open(info_path)).get('transcript', []))
+	transcript.extend(dict(audio_path = info_path.replace('.json', ''), **{k : v for k, v in t.items() if k not in args.strip} ) for t in json.load(open(info_path)).get('transcript', []))
 
 json.dump(transcript, open(args.output_path, 'w'), ensure_ascii = False, indent = 2, sort_keys = True)
 
