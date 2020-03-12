950e92a583d1f0a1fd695d6b91643fd0b4be0d32
Vadim Kantorov <vadimkantorov@gmail.com>
2020-03-12 18:28:49 +0000

    ...

--- audio.py
+++ audio.py
@@ -4,14 +4,17 @@ import librosa
 import torch
 import models
 
-def read_audio(audio_path, sample_rate, offset = 0, duration = None, normalize = True, mono = True, dtype = torch.float32, byte_order = 'little'):
+def read_audio(audio_path, sample_rate, offset = 0, duration = None, normalize = True, mono = True, dtype = torch.float32, byte_order = 'little', backend = 'sox'):
 	try:
 		if audio_path.endswith('.wav'):
 			sample_rate_, signal = scipy.io.wavfile.read(audio_path) 
 			signal = signal[None, :] if len(signal.shape) == 1 else signal.T
-		else:
+		elif backend == 'sox':
 			num_channels = int(subprocess.check_output(['soxi', '-V0', '-c', audio_path])) if not mono else 1
 			sample_rate_, signal = sample_rate, torch.ShortTensor(torch.ShortStorage.from_buffer(subprocess.check_output(['sox', '-V0', audio_path, '-b', '16', '-e', 'signed', '--endian', byte_order, '-r', str(sample_rate), '-c', str(num_channels), '-t', 'raw', '-']), byte_order = byte_order)).reshape(-1, num_channels).t()
+		elif backend == 'ffmpeg':
+			num_channels = int(subprocess.check_output(['ffprobe', '-i', audio_path, '-show_entries', 'stream=channels', '-select_streams', 'a:0', '-of', 'compact=p=0:nk=1', '-v', '0'])) if not mono else 1
+			sample_rate_, signal = sample_rate, torch.ShortTensor(torch.ShortStorage.from_buffer(subprocess.check_output(['ffmpeg', '-i', audio_path, '-nostdin', '-hide_banner', '-nostats', '-loglevel', 'quiet', '-f', 's16le', '-ar', str(sample_rate), '-ac', str(num_channels), '-']), byte_order = byte_order)).reshape(-1, num_channels).t()
 	except:
 		print(f'Error when reading [{audio_path}]')
 		sample_rate_, signal = sample_rate, torch.tensor([[]], dtype = dtype)
