e34ac6a709f29fc4a7725ef509491b70af952906
II245 <II245>
2020-04-17 13:18:27 +0300

    radio setup

--- tools.py
+++ tools.py
@@ -11,6 +11,7 @@ import transcripts
 import datasets
 import metrics
 import ru as lang
+from tqdm import tqdm
 
 def subset(input_path, output_path, audio_name, align_boundary_words, cer, wer, duration, gap, unk, num_speakers):
 	cat = output_path.endswith('.json')
@@ -30,21 +31,22 @@ def subset(input_path, output_path, audio_name, align_boundary_words, cer, wer, 
 		json.dump(transcript_cat, open(output_path, 'w'), ensure_ascii = False, sort_keys = True, indent = 2)
 	print(output_path)
 
-def cut(input_path, output_path, sample_rate, mono, dilate, strip, audio_backend):
+def cut(input_path, output_path, sample_rate, mono, dilate, strip, strip_prefix, audio_backend):
 	os.makedirs(output_path, exist_ok = True)
 	transcript_cat = []
 	
 	transcript = json.load(open(input_path))
 	prev_audio_path, signal = None, None
 
-	for t in sorted(transcript, key = lambda t: t['audio_path']):
+	for t in tqdm(sorted(transcript, key = lambda t: t['audio_path'])):
 		audio_path = t['audio_path']
 		signal = audio.read_audio(audio_path, sample_rate, normalize = False, backend = audio_backend)[0] if audio_path != prev_audio_path else signal
 		t['channel'] = 0 if len(signal) == 1 else None if mono else t.get('channel')
-		segment = signal[t['channel'] if t['channel'] is not None else ..., int(max(t['begin'] - dilate, 0) * sample_rate) : int((t['end'] + dilate) * sample_rate)]
+		segment = signal[slice(t['channel'], 1 + t['channel']) if t['channel'] is not None else ..., int(max(t['begin'] - dilate, 0) * sample_rate) : int((t['end'] + dilate) * sample_rate)]
 		segment_path = os.path.join(output_path, os.path.basename(audio_path) + '.{channel}-{begin:.06f}-{end:.06f}.wav'.format(**t))
 		audio.write_audio(segment_path, segment, sample_rate, mono = True)
-
+		if strip_prefix:
+			segment_path = segment_path.replace('data/', '')
 		t = dict(audio_path = segment_path, channel = 0 if len(signal) == 1 else None, begin = 0.0, end = segment.shape[-1] / sample_rate, speaker = t.pop('speaker', None), ref = t.pop('ref'), hyp = t.pop('hyp', None), cer = t.pop('cer', None), alignment = t.pop('alignment', {}), words = t.pop('words', {}), meta = t)
 		prev_audio_path = audio_path
 		transcript_cat.append(t)
@@ -179,6 +181,7 @@ if __name__ == '__main__':
 	cmd.add_argument('--sample-rate', '-r', type = int, default = 8_000, choices = [8_000, 16_000, 32_000, 48_000])
 	cmd.add_argument('--strip', nargs = '*', default = ['alignment', 'words'])
 	cmd.add_argument('--mono', action = 'store_true')
+	cmd.add_argument('--strip-prefix', action = 'store_true')
 	cmd.add_argument('--audio-backend', default = 'ffmpeg', choices = ['sox', 'ffmpeg'])
 	cmd.set_defaults(func = cut)
 	
