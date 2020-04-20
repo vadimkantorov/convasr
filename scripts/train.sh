a31095908ff5a0bdd5e425f762829c02df6c2867
II245 <II245>
2020-04-20 20:52:17 +0300

    radio open stt 2-4.2 seconds train set

--- scripts/train.sh
+++ scripts/train.sh
@@ -2,15 +2,15 @@ python3 train.py $@ \
   --githttp https://github.com/vadimkantorov/convasr/commit/%h \
   --verbose --lang ru \
   --model JasperNetBig \
-  --train-batch-size 256 --val-batch-size 192 \
+  --train-batch-size 256 --val-batch-size 64 \
   --lr 1e-2 \
   --optimizer NovoGrad \
-  --train-data-path echomsk600/echo_600_train.json \
-  --val-data-path data/clean_val.csv.json echomsk600/echo_600_val.json data/splits/radio_100h_val.csv.json data/mixed_val.csv.json kontur_calls_micro/kontur_calls_micro.csv.json kontur_calls_micro/kontur_calls_micro.0.csv.json kontur_calls_micro/kontur_calls_micro.1.csv.json \
+  --train-data-path data/splits/radio_100h_2-4.2sec_train.json \
+  --val-data-path data/clean_val.csv.json echomsk600/echo_600_val.json data/splits/radio_100h_val.csv.json data/mixed_val.csv.json kontur_calls_micro/kontur_calls_micro.csv.json kontur_calls_micro/kontur_calls_micro.0.csv.json kontur_calls_micro/kontur_calls_micro.1.csv.json data/splits/radio_100h_2-4.2sec_val.json \
   --analyze kontur_calls_micro.csv \
   --val-iteration-interval 2500 \
   --fp16 O2 \
-  --experiment-name radio_100h_cut \
+  --experiment-name radio_100h_ostt2_4 \
   --epochs 100 --checkpoint-skip --exphtml= #\
 # data/mixed_val.csv data/clean_val.csv kontur_calls_micro/kontur_calls_micro.csv kontur_calls_micro/kontur_calls_micro.0.csv kontur_calls_micro/kontur_calls_micro.1.csv 
 #  --val-data-path data/mixed_val.csv data/clean_val.csv kontur_calls_micro/kontur_calls_micro.csv kontur_calls_micro/kontur_calls_micro.0.csv kontur_calls_micro/kontur_calls_micro.1.csv \
