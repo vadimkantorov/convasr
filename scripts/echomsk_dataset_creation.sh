90f67791ab0a2fd7bca8552b36efcda95f9718d2
II245 <II245>
2020-04-21 00:00:13 +0300

    revert echomsk script state to master

--- scripts/echomsk_dataset_creation.sh
+++ scripts/echomsk_dataset_creation.sh
@@ -3,10 +3,10 @@ set -e
 export CUDA_VISIBLE_DEVICES=0
 
 ECHOMSK=data/personalno_20000101_20191231.txt.json.gz
-CHECKPOINT=data/experiments/JasperNetBig_NovoGrad_lr1e-2_wd1e-3_bs256____fp16O2/checkpoint_epoch05_iter0040000.pt
+CHECKPOINT=best_checkpoints/JasperNetBig_NovoGrad_lr1e-4_wd1e-3_bs1024____long-train_bs1024_step6_430k_checkpoint_epoch247_iter0446576.pt
 SAMPLE_RATE=8000
 
-SAMPLE=10
+SAMPLE=1
 DATASET=echomsk$SAMPLE
 DATASET_ROOT=data/$DATASET
 DATASET_AUDIO=$DATASET_ROOT/audio
@@ -14,8 +14,8 @@ DATASET_TRANSCRIBE=$DATASET_ROOT/transcribe
 DATASET_SUBSET=$DATASET_ROOT/subset
 DATASET_CUT=$DATASET_ROOT/cut
 TRANSCRIBE='--mono --align --max-segment-duration 4.0 --html'
-SUBSET='--align-boundary-words --num-speakers 1 --gap 0.1- --cer 0.1-0.4 --duration 2.0-4.0'
-CUT="--dilate 0.02 --sample-rate $SAMPLE_RATE --mono"
+SUBSET='--align-boundary-words --num-speakers 2 --gap 0.15- --cer 0.0-0.15 --duration 2.0-8.0'
+CUT="--dilate 0.075 --sample-rate $SAMPLE_RATE --mono --strip-prefix"
 
 mkdir -p $DATASET_AUDIO
 python3 datasets/echomsk.py -i $ECHOMSK -o $DATASET_AUDIO --sample $SAMPLE
@@ -25,6 +25,7 @@ python3 transcribe.py --checkpoint $CHECKPOINT -i $DATASET_AUDIO -o $DATASET_TRA
 
 python3 tools.py subset -i $DATASET_TRANSCRIBE -o $DATASET_SUBSET.json $SUBSET
 
+rm -r $DATASET_CUT
 python3 tools.py cut -i $DATASET_SUBSET.json -o $DATASET_CUT $CUT
 
-python3 vis.py audiosample -i $DATASET_CUT/$(basename $DATASET_CUT).json -o $DATASET_CUT.json.html 
+#python3 vis.py audiosample -i $DATASET_CUT/$(basename $DATASET_CUT).json -o $DATASET_CUT.json.html 
