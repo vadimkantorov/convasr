e0753a4ce2339ca155f00e3df94d4ce6456a5bc0
II245 <II245>
2020-04-22 01:15:53 +0300

    fix strip-prefix argument

--- scripts/echomsk_dataset_creation.sh
+++ scripts/echomsk_dataset_creation.sh
@@ -14,18 +14,18 @@ DATASET_TRANSCRIBE=$DATASET_ROOT/transcribe
 DATASET_SUBSET=$DATASET_ROOT/subset
 DATASET_CUT=$DATASET_ROOT/cut
 TRANSCRIBE='--mono --align --max-segment-duration 4.0 --html'
-SUBSET='--align-boundary-words --num-speakers 2 --gap 0.15- --cer 0.0-0.15 --duration 2.0-8.0'
-CUT="--dilate 0.075 --sample-rate $SAMPLE_RATE --mono --strip-prefix"
+SUBSET='--align-boundary-words --num-speakers 1 --gap 0.8- --cer 0.0-0.15 --duration 2.0-8.0'
+CUT="--dilate 0.075 --sample-rate $SAMPLE_RATE --mono --strip-prefix data/"
 
-mkdir -p $DATASET_AUDIO
-python3 datasets/echomsk.py -i $ECHOMSK -o $DATASET_AUDIO --sample $SAMPLE
-wget --no-clobber -i $DATASET_AUDIO/audio.txt -P $DATASET_AUDIO
+#mkdir -p $DATASET_AUDIO
+#python3 datasets/echomsk.py -i $ECHOMSK -o $DATASET_AUDIO --sample $SAMPLE
+#wget --no-clobber -i $DATASET_AUDIO/audio.txt -P $DATASET_AUDIO
 
-python3 transcribe.py --checkpoint $CHECKPOINT -i $DATASET_AUDIO -o $DATASET_TRANSCRIBE $TRANSCRIBE
+#python3 transcribe.py --checkpoint $CHECKPOINT -i $DATASET_AUDIO -o $DATASET_TRANSCRIBE $TRANSCRIBE
 
 python3 tools.py subset -i $DATASET_TRANSCRIBE -o $DATASET_SUBSET.json $SUBSET
 
-rm -r $DATASET_CUT
+#rm -r $DATASET_CUT
 python3 tools.py cut -i $DATASET_SUBSET.json -o $DATASET_CUT $CUT
 
 #python3 vis.py audiosample -i $DATASET_CUT/$(basename $DATASET_CUT).json -o $DATASET_CUT.json.html 
