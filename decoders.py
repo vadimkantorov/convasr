233ac96a3e6cb55c5c92bac0fc5fe5db395c7d38
Vadim Kantorov <vadimkantorov@gmail.com>
2020-05-06 18:53:14 +0000

    ...

--- decoders.py
+++ decoders.py
@@ -8,10 +8,7 @@ class BeamSearchDecoder:
 	def __init__(self, labels, lm_path, beam_width, beam_alpha = 0, beam_beta = 0, cutoff_top_n = 40, cutoff_prob = 1.0, num_workers = 1, topk = 1):
 		import ctcdecode
 		self.topk = topk
-		labels_with_spacebug_workaround = list(str(labels).lower())
-		if labels_with_spacebug_workaround.count(' ') == 2:
-			labels_with_spacebug_workaround[labels_with_spacebug_workaround.index(' ')] = '_'
-		self.beam_search_decoder = ctcdecode.CTCBeamDecoder(labels_with_spacebug_workaround, lm_path, beam_alpha, beam_beta, cutoff_top_n if cutoff_top_n is not None else len(labels), cutoff_prob, beam_width, num_workers, labels.blank_idx, log_probs_input = True)
+		self.beam_search_decoder = ctcdecode.CTCBeamDecoder(list(str(labels).lower()), lm_path, beam_alpha, beam_beta, cutoff_top_n if cutoff_top_n is not None else len(labels), cutoff_prob, beam_width, num_workers, labels.blank_idx, log_probs_input = True)
 
 	def decode(self, log_probs, output_lengths):
 		list_or_one = lambda xs: xs if len(xs) > 1 else xs[0]
