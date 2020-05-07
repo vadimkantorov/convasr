7ad5309d59afa45c136085f349b57bca6694d974
II245 <II245>
2020-05-07 17:01:34 +0300

    imporovements for transcribe py

--- scripts/transcribe_for_assessment.sh
+++ scripts/transcribe_for_assessment.sh
@@ -1,4 +1,5 @@
 python3 transcribe.py $@ \
-	-i data/to_transcribe/chunk_10 -o data/to_transcribe/chunk_10_out \
-        --checkpoint best_checkpoints/JasperNetBig_NovoGrad_lr5e-5_wd1e-3_bs32____finetune_kfold_long_train_long_ftune_1603_0_checkpoint_epoch206_iter0248969.pt \
-	--txt --mono
+	-i /home/yuborovskikh/work/dataset_tools/chunk_12/samples -o /home/yuborovskikh/work/dataset_tools/chunk_12/transcripts \
+         --mono --checkpoint best_checkpoints/JasperNetBig_NovoGrad_lr5e-5_wd1e-3_bs32____finetune_kfold_long_train_long_ftune_1604_0_checkpoint_epoch326_iter0534734.pt --txt 
+
+
