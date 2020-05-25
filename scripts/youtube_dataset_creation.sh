67d6b956b007b3e7586d9970559b08dc07848fed
11245 <11245>
2020-05-26 00:32:07 +0500

    update sh file and examples exclude and shuffle

--- scripts/youtube_dataset_creation.sh
+++ scripts/youtube_dataset_creation.sh
@@ -1,6 +1,6 @@
 set -e 
 
-export CUDA_VISIBLE_DEVICES=1
+export CUDA_VISIBLE_DEVICES=0,1
 
 YOUTUBE='https://www.youtube.com/channel/UCb9_Bhv37NXN1m8Bmrm9x9w'
 CHECKPOINT=best_checkpoints/JasperNetBig_NovoGrad_lr1e-4_wd1e-3_bs1024____long-train_bs1024_step6_430k_checkpoint_epoch247_iter0446576.pt
@@ -14,9 +14,10 @@ DATASET_TRANSCRIBE=$DATASET_ROOT/transcribe
 DATASET_SUBSET=$DATASET_ROOT/subset
 DATASET_CUT=$DATASET_ROOT/cut
 
-TRANSCRIBE='--mono --align --max-segment-duration 4.0 --html'
-SUBSET='--align-boundary-words --num-speakers 1 --gap 0.1- --cer 0.1-0.4 --duration 2.0-4.0'
-CUT="--dilate 0.02 --sample-rate $SAMPLE_RATE --mono"
+TRANSCRIBE='--mono --batch-time-padding-multiple 1 --align --skip-processed --max-segment-duration 4.0  --skip-file-longer-than-hours 0.8'
+SUBSET='--num-speakers 1 --gap 0.05- --cer 0.0-0.15 --duration 0.5-8.0'
+CUT="--dilate 0.025 --sample-rate $SAMPLE_RATE --mono --strip-prefix data/ --add-sub-paths --strip"
+TRAIN_TEST_SPLIT='--test-duration-in-hours 10'
 
 #mkdir -p $DATASET_AUDIO
 #bash datasets/youtube.sh LIST "$YOUTUBE" > $DATASET_ROOT/youtube.txt
@@ -24,10 +25,14 @@ CUT="--dilate 0.02 --sample-rate $SAMPLE_RATE --mono"
 #head -n $SAMPLE $DATASET_ROOT/youtube.txt > $DATASET_AUDIO/audio.txt # list of links like http://youtu.be/e8bkFQD3ZhA
 #bash datasets/youtube.sh RETR $DATASET_AUDIO/audio.txt $DATASET_AUDIO
 
-#python3 datasets/youtube.py -i $DATASET_AUDIO -o $DATASET_AUDIO
+#python3 datasets/youtube.py -i $DATASET_AUDIO -o $DATASET_AUDIO.json
 
-python3 transcribe.py --checkpoint $CHECKPOINT -i $DATASET_AUDIO -o $DATASET_TRANSCRIBE $TRANSCRIBE
+python3 transcribe.py --checkpoint $CHECKPOINT -i $DATASET_AUDIO.json -o $DATASET_TRANSCRIBE $TRANSCRIBE
 
 #python3 tools.py subset -i $DATASET_TRANSCRIBE -o $DATASET_SUBSET.json $SUBSET
+
 #python3 tools.py cut -i $DATASET_SUBSET.json -o $DATASET_CUT $CUT
+
+#python3 tools.py split -i $DATASET_CUT_JSON -o $DATASET_CUT $TRAIN_TEST_SPLIT
+
 #python3 vis.py audiosample -i $DATASET_CUT/$(basename $DATASET_CUT).json -o $DATASET_CUT.json.html
