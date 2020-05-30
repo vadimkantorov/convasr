801288d1573ce510a88c141d9b18690ae2ab6311
Vadim Kantorov <vadimkantorov@gmail.com>
2020-05-30 16:46:54 +0000

    ...

--- datasets/echomsk.py
+++ datasets/echomsk.py
@@ -9,12 +9,14 @@ parser.add_argument('--input_path', '-i', required = True)
 parser.add_argument('--output-path', '-o', required = True)
 parser.add_argument('--min-speakers', type = int, default = 2)
 parser.add_argument('--max-speakers', type = int, default = 2)
+parser.add_argument('--min-seconds', type = float, default = 1)
+parser.add_argument('--max-seconds', type = float, default = 60 * 60)
 parser.add_argument('--sample', type = int, default = 10)
 args = parser.parse_args()
 
 gzopen = lambda file_path, mode = 'r': gzip.open(file_path, mode + 't') if file_path.endswith('.gz') else open(file_path, mode)
 episodes = json.load(gzopen(args.input_path))
-episodes = [e for e in episodes if args.min_speakers <= len(e['speakers']) <= args.max_speakers and e['sound_seconds'] > 0 and len(e['sound']) == 1]
+episodes = [e for e in episodes if args.min_speakers <= len(e['speakers']) <= args.max_speakers and args.min_seconds <= e['sound_seconds'] <= args.max_seconds and len(e['sound']) == 1]
 random.seed(1)
 random.shuffle(episodes)
 episodes = episodes[:args.sample]
