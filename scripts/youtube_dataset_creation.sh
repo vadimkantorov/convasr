382c01d05e986134d92c09d5fc0a012c23513256
11245 <11245>
2020-06-29 22:15:04 +0500

    update youtube_dataset_creation

--- scripts/youtube_dataset_creation.sh
+++ scripts/youtube_dataset_creation.sh
@@ -16,13 +16,14 @@ DATASET_AUDIO=$DATASET_ROOT/audio
 DATASET_TRANSCRIBE=$DATASET_ROOT/transcribe
 DATASET_SUBSET=$DATASET_ROOT/subset
 DATASET_CUT=$DATASET_ROOT/cut
-DATASET_CUT_JSON=$DATASET_ROOT/cut/cut.json
+DATASET_CUT_JSON=$DATASET_ROOT/cut/cut2.json
+
 
 TRANSCRIBE='--mono --batch-time-padding-multiple 1 --align --skip-processed --max-segment-duration 4.0  --skip-file-longer-than-hours 2.0'
 SUBSET='--num-speakers 1 --gap 0.05- --cer 0.0-0.25 --duration 0.5-8.0'
 CUT="--dilate 0.025 --sample-rate $SAMPLE_RATE --mono --strip-prefix data/ --add-sub-paths --strip"
-TRAIN_TEST_SPLIT='--microval-duration-in-hours 10 --val-duration-in-hours 0 --test-duration-in-hours 0'
-
+TRAIN_TEST_SPLIT='--microval-duration-in-hours 0 --val-duration-in-hours 0 --test-duration-in-hours 0 --old-microval-path cut_microval_10h.json'
+TRANSCRIPT_PREPROC='--skip-transcript-large-than-char 45000 --split-by-parts 3 --skip-files-longer-than-hours 2'
 
 #mkdir -p $DATASET_AUDIO
 #bash datasets/youtube.sh LIST "$YOUTUBE" > $DATASET_ROOT/youtube.txt
@@ -30,11 +31,11 @@ TRAIN_TEST_SPLIT='--microval-duration-in-hours 10 --val-duration-in-hours 0 --te
 #head -n $SAMPLE $DATASET_ROOT/youtube.txt > $DATASET_AUDIO/audio.txt # list of links like http://youtu.be/e8bkFQD3ZhA
 #bash datasets/youtube.sh RETR $DATASET_AUDIO/audio.txt $DATASET_AUDIO
 
-python3 datasets/youtube.py -i $DATASET_AUDIO -o $DATASET_AUDIO.json --split-by-parts 3
+#python3 datasets/youtube.py -i $DATASET_AUDIO -o $DATASET_AUDIO.json $TRANSCRIPT_PREPROC
 
 python3 transcribe.py --checkpoint $CHECKPOINT -i $DATASET_AUDIO.json -o $DATASET_TRANSCRIBE $TRANSCRIBE
 
-#python3 tools.py subset -i $DATASET_TRANSCRIBE -o $DATASET_SUBSET.json $SUBSET
+python3 tools.py subset -i $DATASET_TRANSCRIBE -o $DATASET_SUBSET.json $SUBSET
 
 #python3 tools.py cut -i $DATASET_SUBSET.json -o $DATASET_CUT $CUT
 
