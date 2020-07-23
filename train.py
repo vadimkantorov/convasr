75c8af95c3ae5a25825ab9a4a22467fceae60574
Squire-tomsk <abuca9268@gmail.com>
2020-07-23 18:20:43 +0300

    Model config dump is added (#13)
    * Do not load optimizer state dict if saved dict is none
    
    * Several configurations are added
    
    * Model config dump is added
    
    Co-authored-by: Alexander Abugaliev <abugaliev@skbkontur.ru>

--- train.py
+++ train.py
@@ -424,7 +424,7 @@ def main(args):
 		model.parameters(), lr = args.lr, betas = args.betas, weight_decay = args.weight_decay
 	) if args.optimizer == 'FusedNovoGrad' else None
 
-	if checkpoint:
+	if checkpoint and checkpoint['optimizer_state_dict'] is not None:
 		optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
 		if not args.skip_optimizer_reset:
 			optimizers.reset_options(optimizer)
@@ -461,6 +461,12 @@ def main(args):
 	performance_meter = metrics.PerformanceMeter()
 	with open(os.path.join(args.experiment_dir, args.args), 'w') as f:
 		json.dump(vars(args), f, sort_keys = True, ensure_ascii = False, indent = 2)
+
+	with open(os.path.join(args.experiment_dir, args.dump_model_config), 'w') as f:
+		model_config = {
+			'init_params': models.master_module(model).init_params, 'model': repr(models.master_module(model))
+		}
+		json.dump(model_config, f, sort_keys = True, ensure_ascii = False, indent = 2)
 
 	tic, toc_fwd, toc_bwd = time.time(), time.time(), time.time()
 	loss_avg, entropy_avg, time_ms_avg, lr_avg = 0.0, 0.0, 0.0, 0.0
@@ -607,6 +613,9 @@ if __name__ == '__main__':
 	)
 	parser.add_argument('--logits-topk', type = int)
 	parser.add_argument('--args', default = 'args.json', help = 'save experiment arguments to the experiment dir')
+	parser.add_argument(
+		'--dump-model-config', default = 'model.json', help = 'save model configuration to the experiment dir'
+	)
 	parser.add_argument('--model', default = 'JasperNetBig')
 	parser.add_argument(
 		'--seed',
