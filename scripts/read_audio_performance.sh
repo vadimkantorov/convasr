a63956fdb3cca096579056df9efbb1eba2e5270b
11245 <11245>
2020-08-13 14:29:09 +0500

    added echo

--- scripts/read_audio_performance.sh
+++ scripts/read_audio_performance.sh
@@ -3,6 +3,8 @@
 # ffmpeg -f lavfi -i "sine=frequency=1000:duration=60" -c:a pcm_s16le -ar 8000 data/tests/test_1m.wav
 # ffmpeg -f lavfi -i "sine=frequency=1000:duration=5" -c:a pcm_s16le -ar 8000 data/tests/test_5s.wav
 
+echo "| file       | reads count |  backend | process_time us| perf_counter us|"
+echo "|-----------:|---:|---------:|--------------:|-------------:|"
 for backend in sox ffmpeg soundfile scipy; do
   for file in test_5s.wav test_1m.wav test_1h.wav; do
     python audio.py timeit --audio-path data/tests/$file --mono --audio-backend $backend
