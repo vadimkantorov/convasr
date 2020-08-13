4de8aa620416b3a3eab68a1890f29642b490a89d
Vadim Kantorov <vadimkantorov@gmail.com>
2020-08-13 15:52:28 +0000

    ...

--- audio.py
+++ audio.py
@@ -1,13 +1,15 @@
 import subprocess
-import scipy.io.wavfile
-import librosa
-import torch
-import models
 import time
+import torch
+import numpy as np
+import librosa
 import soundfile
+import scipy.io.wavfile
+import models
 
-f2s = lambda signal: (signal * torch.iinfo(torch.int16).max).short()
-s2f = lambda signal: signal.float() / torch.iinfo(torch.int16).max
+smax = torch.iinfo(torch.int16).max
+f2s_numpy = lambda signal, max = np.float32(smax): np.multiply(signal, max, dtype = 'int16')
+s2f_numpy = lambda signal, max = np.float32(smax): np.divide(signal, max, dtype = 'float32')
 
 
 def read_audio(
@@ -28,15 +30,15 @@ def read_audio(
 			if audio_path is not None:
 				with open(audio_path, 'rb') as f:
 					raw_s16le = f.read()
-			sample_rate_, signal = raw_sample_rate, torch.ShortTensor(torch.ShortStorage.from_buffer(raw_s16le, byte_order = byte_order)).reshape(-1, raw_num_channels).t()
+			sample_rate_, signal = raw_sample_rate, np.frombuffer(raw_s16le, dtype = 'int16').reshape(-1, raw_num_channels).T
 
 		elif backend in ['scipy', None] and audio_path.endswith('.wav'):
 			sample_rate_, signal = scipy.io.wavfile.read(audio_path)
-			signal = torch.as_tensor(signal[None, :] if len(signal.shape) == 1 else signal.T)
+			signal = signal[None, :] if len(signal.shape) == 1 else signal.T
 
 		elif backend == 'soundfile':
 			signal, sample_rate_ = soundfile.read(audio_path, dtype = 'int16')
-			signal = torch.as_tensor(signal[None, :] if len(signal.shape) == 1 else signal.T)
+			signal = signal[None, :] if len(signal.shape) == 1 else signal.T
 
 		elif backend == 'sox':
 			num_channels = int(subprocess.check_output(['soxi', '-V0', '-c', audio_path])) if not mono else 1
@@ -58,9 +60,7 @@ def read_audio(
 				'raw',
 				'-'
 			]
-			sample_rate_, signal = sample_rate, \
-                                        torch.ShortTensor(torch.ShortStorage.from_buffer(subprocess.check_output(
-				params), byte_order = byte_order)).reshape(-1, num_channels).t()
+			sample_rate_, signal = sample_rate, np.frombuffer(subprocess.check_output(params), dtype = 'int16').reshape(-1, num_channels).T
 		elif backend in ['ffmpeg', None]:
 			num_channels = int(
 				subprocess.check_output([
@@ -94,24 +94,26 @@ def read_audio(
 				str(num_channels),
 				'-'
 			]
-			sample_rate_, signal = sample_rate, \
-                                        torch.ShortTensor(torch.ShortStorage.from_buffer(subprocess.check_output(params), byte_order = byte_order)).reshape(-1, num_channels).t()
+			sample_rate_, signal = sample_rate, np.frombuffer(subprocess.check_output(params), dtype = 'int16').reshape(-1, num_channels).T
 
 	except:
 		print(f'Error when reading [{audio_path}]')
-		sample_rate_, signal = sample_rate, torch.tensor([[]], dtype = torch.int16)
+		sample_rate_, signal = sample_rate, np.array([[]], dtype = 'int16')
 
-	assert signal.dtype in [torch.int16, torch.float32]
-	if signal.dtype is torch.int16:
-		signal = s2f(signal)
 	if offset or duration is not None:
 		signal = signal[...,
 						slice(
 							int(offset * sample_rate_) if offset else None,
 							int((offset + duration) * sample_rate_) if duration is not None else None
 						)]
-	if mono:
-		signal = signal.mean(dim = 0, keepdim = True)
+	
+	assert 'int16' in str(signal.dtype) or 'float32' in str(signal.dtype) 
+	if 'int16' in str(signal.dtype):
+		signal = s2f_numpy(signal)
+	if mono and len(signal) > 1:
+		signal = signal.mean(0, keepdims = True)
+
+	signal = torch.as_tensor(signal)
 	if normalize:
 		signal = models.normalize_signal(signal, dim = -1)
 	if sample_rate_ != sample_rate:
@@ -123,7 +125,7 @@ def read_audio(
 def write_audio(audio_path, signal, sample_rate, mono = False):
 	assert signal.dtype is torch.float32
 	signal = signal if not mono else signal.mean(dim = 0, keepdim = True)
-	scipy.io.wavfile.write(audio_path, sample_rate, f2s(signal.t()).numpy())
+	scipy.io.wavfile.write(audio_path, sample_rate, f2s_numpy(signal.t().numpy()))
 	return audio_path
 
 
@@ -138,10 +140,13 @@ def compute_duration(audio_path, backend = 'ffmpeg'):
 
 
 def timeit(audio_path, number, sample_rate, mono, audio_backend, scale):
+	for i in range(3):
+		read_audio(audio_path, sample_rate = sample_rate, mono = mono, backend = audio_backend, normalize=False)
+	
 	start_process_time = time.process_time_ns()
 	start_perf_counter = time.perf_counter_ns()
 	for i in range(number):
-		signal, sample_rate = read_audio(audio_path, sample_rate = sample_rate, mono = mono, backend = audio_backend, normalize=False)
+		read_audio(audio_path, sample_rate = sample_rate, mono = mono, backend = audio_backend, normalize=False)
 	end_process_time = time.process_time_ns()
 	end_perf_counter = time.perf_counter_ns()
 	process_time = (end_process_time - start_process_time) / scale / number
