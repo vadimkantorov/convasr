f44038fbb604989803a465b0dfb4d3dd5e781299
Vadim Kantorov <vadimkantorov@gmail.com>
2020-09-17 08:58:40 +0000

    diarization work

---
+++ scripts/diarization.sh
@@ -0,0 +1,4 @@
+python3 diarization.py -i diarization/mono -o data/diarization 
+
+#python3 diarization.py -i b70a59ea-5caa-4783-9bb7-32b4eceec16a.mp3.wav -o data/diarization.json
+#python3 vis.py transcript --mono -i data/diarization.json -o data/diarization.html
