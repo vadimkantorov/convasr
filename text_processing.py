fa69564a2fc9f7b79d9cc95ceeab041b7f8a970c
AlexanderAbugaliev <abuca9268@gmail.com>
2020-11-11 16:07:22 +0300

    text normalization is added (#53)

--- text_processing.py
+++ text_processing.py
@@ -104,6 +104,7 @@ class TextProcessor:
 			return text
 		else:
 			text = re.sub(rf'[^{self.allowed_chars}]', '', text)
+			text = re.sub('\s2', ' ', text)
 			text = re.sub('\s+', ' ', text)
 			return text
 
@@ -111,18 +112,22 @@ class TextProcessor:
 class TextPreprocessor(TextProcessor):
 	def __init__(self,
 				 repeat_character: str = None,
+	             normalize_text: bool = False,
 				 **kwargs):
 		super().__init__(**kwargs)
 		self.repeat_character = repeat_character #if not none all doubled chars will be replaced by single char and repeat char
 
+		self.handle_normalize = TextNormalizer().normalize if normalize_text else lambda text: text
+
 		self.handlers = [
-			self.handle_strip,
+			self.handle_normalize,
 			self.handle_case,
 			self.handle_repeat,
 			self.handle_collapse,
 			self.handle_drop,
 			self.handle_replace,
-			self.handle_allowed
+			self.handle_allowed,
+			self.handle_strip
 		]
 
 	def handle_repeat(self, text):
@@ -139,13 +144,13 @@ class TextPostprocessor(TextProcessor):
 		self.repeat_character = repeat_character
 
 		self.handlers = [
-			self.handle_strip,
 			self.handle_case,
 			self.handle_collapse,
 			self.handle_drop,
 			self.handle_repeat,
 			self.handle_replace,
-			self.handle_allowed
+			self.handle_allowed,
+			self.handle_strip
 		]
 
 	def handle_repeat(self, text):
@@ -158,3 +163,138 @@ class TextPostprocessor(TextProcessor):
 			else:
 				result.append(text[i])
 		return ''.join(result)
+
+
+class TextNormalizer:
+	def __init__(self):
+		self._minus = 'минус'
+		self._percent = 'процент'
+
+		self._arabic2roman = {
+			1000: 'M',
+			900 : 'CM',
+			500 : 'D',
+			400 : 'CD',
+			100 : 'C',
+			90  : 'XC',
+			50  : 'L',
+			40  : 'XL',
+			10  : 'X',
+			9   : 'IX',
+			5   : 'V',
+			4   : 'IV',
+			1   : 'I'
+		}
+
+		self._roman2arabic = {}
+		for i in range(1,31):
+			res = ''
+			x = i
+			for a, r in sorted(self._arabic2roman.items(), reverse = True):
+				cnt = int(x / a)
+				res += r * cnt
+				x -= a * cnt
+			self._roman2arabic[res] = i
+
+		self._ordinalcardinal2text = {
+			0         : ('ноль', 'нулевой'),
+			1         : ('один', 'первый'),
+			2         : ('два', 'второй'),
+			3         : ('три', 'третий'),
+			4         : ('четыре', 'четвертый'),
+			5         : ('пять', 'пятый'),
+			6         : ('шесть', 'шестой'),
+			7         : ('семь', 'седьмой'),
+			8         : ('восемь', 'восьмой'),
+			9         : ('девять', 'девятый'),
+			10        : ('десять', 'десятый'),
+			11        : ('одиннадцать', 'одиннадцатый'),
+			12        : ('двенадцать', 'двенадцатый'),
+			13        : ('тринадцать', 'тринадцатый'),
+			14        : ('четырнадцать', 'четырнадцатый'),
+			15        : ('пятнадцать', 'пятнадцатый'),
+			16        : ('шестнадцать', 'шестнадцатый'),
+			17        : ('семнадцать', 'семнадцатый'),
+			18        : ('восемнадцать', 'восемнадцатый'),
+			19        : ('девятнадцать', 'девятнадцатsq'),
+			20        : ('двадцать', 'двадцатый'),
+			30        : ('тридцать', 'тридцатый'),
+			40        : ('сорок', 'сороковой'),
+			50        : ('пятьдесят', 'пятьдесятый'),
+			60        : ('шестьдесят', 'шестьдесятый'),
+			70        : ('семьдесят', 'семидесятый'),
+			80        : ('восемьдесят', 'восемьдесятый'),
+			90        : ('девяносто', 'девяностый'),
+			100       : ('сто', 'сотый'),
+			200       : ('двести', 'двухсотый'),
+			300       : ('триста', 'трехсотый'),
+			400       : ('четыреста', 'четырехсотый'),
+			500       : ('пятьсот', 'пятисотый'),
+			600       : ('шестьсот', 'шестисотый'),
+			700       : ('семьсот', 'семисотый'),
+			800       : ('восемьсот', 'восьмисотый'),
+			900       : ('девятьсот', 'десятисотый'),
+			1000      : ('тысяча', 'тысячный'),
+			1000000   : ('миллион', 'миллионный'),
+			1000000000: ('миллиард', 'миллиардный'),
+		}
+
+	def normalize(self, text):
+		# superscripts
+		superscripts = '⁰¹²³⁴⁵⁶⁷⁸⁹'
+		text = re.sub(f'[{superscripts}]', ' ', text)
+
+		# percent isnt preserved
+		text = text.replace('%', f' {self._percent}*')
+
+		# ignores punct
+		# extract words, numbers, ordinal numbers
+		words = re.findall(r'-?\d+-\w+|-?\d+\.?\d*|[\w*]+', text)
+		text = ' '.join(map(self.preprocess_word, words))
+		return text
+
+	def preprocess_word(self, word):
+		if word in self._roman2arabic:
+			word = str(self._roman2arabic[word])
+
+		w0, (w1, w2) = word[0], (word[1:].split('-', 1) + [''])[:2]
+
+		is_num = (w0 == '-' or w0.isdigit()) and (not w1 or w1.isdigit())
+		is_ordinal = w2 and not w2.isdigit()
+		if is_num:
+			word = self.arabic2text(w0 + w1, ordinal = is_ordinal)
+
+		return word
+
+	def arabic2text(self, num, ordinal = False):
+		num = int(num)
+		res = []
+		if num < 0:
+			res.append((self._minus, self._minus))
+			num *= -1
+
+		for a, r, in sorted(self._ordinalcardinal2text.items(), reverse = True):
+			if num >= a:
+				div = num // a if a > 0 else 0
+				if div > 1:
+					res.extend(self.arabic2text(div, ordinal = None))
+				res.append(r)
+				num -= div * a
+				if num == 0:
+					break
+
+		return res if ordinal is None else ' '.join(
+			tuple(zip(*res))[0] if not ordinal else list(tuple(zip(*res))[0])[:-1] + [res[-1][1]]
+		)
+
+
+# if __name__ == '__main__':
+# 	import argparse
+# 	parser = argparse.ArgumentParser()
+# 	parser.add_argument(
+# 		'--text', default = '1-й Здорово http://echomsk.ru/programs/-echo 2.5 оу 1ого 100% XIX век XX-й век -4 13.06'
+# 	)
+# 	args = parser.parse_args()
+# 	text_config = '/work/asr_language_model/decoder/acoustic/configs/ru_text_config.json'
+# 	print('ORIG:', repr(args.text))
+# 	print('NORM:', repr(normalize_text(args.text)))
