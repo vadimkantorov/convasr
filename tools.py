575c7052c5516e28f0d5de335c9fe5bdcbcf0b3e
11245 <11245>
2020-11-13 16:10:30 +0500

    added new tool for dataset filtering, added fix for sampler init, change vis tabulate order

--- tools.py
+++ tools.py
@@ -430,6 +430,50 @@ def processcomments(input_path, output_path, comment_path):
 	#print('\n'.join('{w},{be},{be_}'.format(w = w, be = not_word_stats[w]['be'], be_ = not_word_stats[w]['b'] + not_word_stats[w]['e']) for w in not_word))
 
 
+def filter_dataset(input_path,
+		output_path,
+		duration_in_hours,
+		cer,
+		seed):
+	dataset = json.load(open(input_path))
+
+	random.seed(seed)
+	random.shuffle(dataset)
+
+	if cer:
+		dataset = [e for e in dataset if e['cer'] <= cer]
+
+	print('after cer filtering: ', sum(transcripts.compute_duration(t, hours=True) for t in dataset), 'hours')
+
+	if duration_in_hours is not None:
+		s = []
+		set_duration = 0
+		while set_duration <= duration_in_hours and len(dataset) > 0:
+			t = dataset.pop()
+			set_duration += transcripts.compute_duration(t, hours = True)
+			s.append(t)
+
+		print('final: ', sum(transcripts.compute_duration(t, hours = True) for t in s), 'hours')
+		print(output_path)
+		json.dump(
+			s,
+			open(output_path, 'w'),
+			ensure_ascii = False,
+			sort_keys = True,
+			indent = 2
+		)
+	else:
+		print('final: ', sum(transcripts.compute_duration(t, hours=True) for t in dataset), 'hours')
+		print(output_path)
+		json.dump(
+			dataset,
+			open(output_path, 'w'),
+			ensure_ascii = False,
+			sort_keys = True,
+			indent = 2
+		)
+
+
 def split(
 	input_path,
 	output_path,
@@ -589,6 +633,14 @@ if __name__ == '__main__':
 	cmd.add_argument('--microval-duration-in-hours', required = True, type = float)
 	cmd.add_argument('--seed', required = True, type = int, default = 42)
 	cmd.set_defaults(func = split)
+
+	cmd = subparsers.add_parser('filter_dataset')
+	cmd.add_argument('--input-path', '-i', required = True)
+	cmd.add_argument('--output-path', '-o', required = True)
+	cmd.add_argument('--duration-in-hours', required = False, type = float)
+	cmd.add_argument('--cer', required = False, type = float)
+	cmd.add_argument('--seed', required = False, type = int, default = 42)
+	cmd.set_defaults(func = filter_dataset)
 
 	cmd = subparsers.add_parser('processcomments')
 	cmd.add_argument('--input-path', '-i', required = True)
