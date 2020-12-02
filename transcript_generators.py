e36eb7550544f093e0ab936033fa96874c114c32
Alexander Abugaliev <abugaliev@skbkontur.ru>
2020-12-02 19:31:13 +0300

    Generator speedup

--- transcript_generators.py
+++ transcript_generators.py
@@ -14,21 +14,23 @@ class GreedyCTCGenerator:
 	             segment_text_key: str = 'hyp',
 	             segment_extra_info: typing.List[dict] = None) -> typing.List[typing.List[transcripts.Transcript]]:
 		_transcripts = []
-		most_probable_idx = log_probs.argmax(dim = 1)
+		most_probable_idx = log_probs.argmax(dim = 1).cpu().tolist()
+		begin = F.relu(begin).cpu().tolist() if time_stamps is not None else begin.cpu().tolist()
+		end = end.cpu().tolist()
 		for i in range(len(most_probable_idx)):
 			sample_idx = most_probable_idx[i]
 			sample_len = output_lengths[i] if output_lengths is not None else len(most_probable_idx[i])
-			sample_ts = time_stamps[i] if time_stamps is not None else None
+			sample_ts = time_stamps[i].cpu().tolist() if time_stamps is not None else None
 			transcript = transcripts.Transcript()
 
 			t = 0
-			while t < len(sample_idx) and sample_idx[t].item() in tokenizer.silence_tokens_ids:
+			while t < len(sample_idx) and sample_idx[t] in tokenizer.silence_tokens_ids:
 				t += 1
 			if t >= len(sample_idx):
 				_transcripts.append([transcript])
 				continue
 			tokens = [tokenizer.eps_id]
-			time_begin = F.relu_(begin[i]) + sample_ts[t] if sample_ts is not None else begin[i]
+			time_begin = begin[i] + sample_ts[t] if sample_ts is not None else begin[i]
 			time_end = end[i]
 
 			allow_tokens_repeat = False
@@ -39,22 +41,22 @@ class GreedyCTCGenerator:
 				elif sample_idx[t] == tokens[-1] and not allow_tokens_repeat:
 					continue
 				if tokenizer.is_start_word_token(sample_idx[t]) and sample_ts is not None:
-					segment = transcripts.Segment(begin = time_begin.item(),
-					                              end = time_end.item(),
+					segment = transcripts.Segment(begin = time_begin,
+					                              end = time_end,
 					                              **{segment_text_key: tokenizer.decode([tokens[1:]])[0]})
 					if segment_extra_info is not None:
 						segment.update(segment_extra_info[i])
 					transcript.append(segment)
 					tokens = [tokenizer.eps_id, sample_idx[t]]
-					time_begin = F.relu_(begin[i]) + sample_ts[t] if sample_ts is not None else begin[i]
+					time_begin = begin[i] + sample_ts[t] if sample_ts is not None else begin[i]
 
 				allow_tokens_repeat = False
 				tokens.append(sample_idx[t])
-				time_end = F.relu_(begin[i]) + sample_ts[t] if sample_ts is not None else end[i]
+				time_end = begin[i] + sample_ts[t] if sample_ts is not None else end[i]
 
 			if len(tokens) > 1:
-				segment = transcripts.Segment(begin = time_begin.item(),
-				                              end = time_end.item(),
+				segment = transcripts.Segment(begin = time_begin,
+				                              end = time_end,
 				                              **{segment_text_key: tokenizer.decode([tokens[1:]])[0]})
 				if segment_extra_info is not None:
 					segment.update(segment_extra_info[i])
