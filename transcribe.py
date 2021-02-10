36ea3ee12179a1a37e51b1a05ad9c8cd9fb26399
11245 <11245>
2021-02-11 02:04:02 +0500

    move prints

--- transcribe.py
+++ transcribe.py
@@ -131,10 +131,11 @@ def main(args, ext_json = ['.json', '.json.gz']):
 
 		try:
 			tic = time.time()
-			y, ylen = y.to(args.device), ylen.to(args.device)
 			print('Input path:', meta[0]['audio_path'])
-			print('Input len: {length:.02f} sec'.format(length = x.shape[-1] / args.sample_rate if args.frontend else x.shape[-1] * args.window_stride))
+			print('Input len: {length:.02f} sec'.format(
+					length=x.shape[-1] / args.sample_rate if args.frontend else x.shape[-1] * args.window_stride))
 
+			y, ylen = y.to(args.device), ylen.to(args.device)
 			log_probs, logits, olen = model(x.squeeze(1).to(args.device), xlen.to(args.device))
 
 			print('Input time steps:', log_probs.shape[-1], '| target time steps:', y.shape[-1])
