b5c54a78c6da32e6fa610de84c3bae3da4829d9a
11245 <11245>
2021-02-25 02:02:57 +0500

    bash prof setups

---
+++ scripts/profile_one_layer_pytorch_model_wo_warmup.sh
@@ -0,0 +1,18 @@
+set -e
+
+TRACEFILE=profile_one_layer_pytorch.sqlite
+TRACELOG=profile_one_layer_pytorch.txt
+TRACEPYPROFLOG=profile_one_layer_pytorch.pyprof.txt
+CUDNN_LOGDEST=profile_one_layer_pytorch_cudnn_dbg.txt
+CUBLAS_LOGDEST=profile_one_layer_pytorch_cublas_dbg.txt
+
+CUDA_VISIBLE_DEVICES=0 CUDNN_LOGINFO_DBG=1 CUDNN_LOGDEST_DBG=$CUDNN_LOGDEST CUBLAS_LOGINFO_DBG=1 CUBLAS_LOGDEST_DBG=$CUBLAS_LOGDEST nvprof -f -o $TRACEFILE -s --devices 0 --profile-from-start off -- python3 benchmark.py \
+  --fp16 O2 \
+  --model ProfilableModel \
+  --iterations 1 \
+  --iterations-warmup 0 \
+  --profile-cuda \
+  -B 32 \
+  -T 16 &> $TRACELOG
+
+python3 scripts/nvprof2json.py $TRACEFILE > $TRACEFILE.json
