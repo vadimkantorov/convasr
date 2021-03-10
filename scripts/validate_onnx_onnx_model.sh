369cf3a39aeddf6b899c22c965a0b14fc36fe1eb
11245 <11245>
2021-03-10 13:51:46 +0300

    update scripts

--- scripts/validate_onnx_onnx_model.sh
+++ scripts/validate_onnx_onnx_model.sh
@@ -8,6 +8,6 @@ python3 train.py $@ \
   --num-workers 32 \
   --val-batch-size 256 \
   --checkpoint best_checkpoints/JasperNetBig_NovoGrad_lr1e-4_wd1e-3_bs512____rerun_finetune_after_self_train_epoch183_iter0290000.pt \
-  --val-data-path domain_set/transcripts/16082020/valset_16082020.0.csv.json
+  --val-data-path domain_set/transcripts/16082020/valset_16082020.0.csv.json domain_set/transcripts/16082020/valset_16082020.1.csv.json
 
 #best_checkpoints/JasperNetBig_NovoGrad_lr1e-4_wd1e-3_bs256____youtube_3000h_finetune_2_checkpoint_epoch448_iter0435000.onnx
--- scripts/validate_onnx_onnx_model_fp16.sh
+++ scripts/validate_onnx_onnx_model_fp16.sh
@@ -4,8 +4,8 @@ python3 train.py $@ \
   --device cuda \
   --onnx-validate \
   --frontend-in-model \
-  --onnx best_checkpoints/JasperNetBig_NovoGrad_lr1e-4_wd1e-3_bs512____rerun_finetune_after_self_train_epoch183_iter0290000.pt.12.fp16.onnx \
+  --onnx best_checkpoints/JasperNetBig_NovoGrad_lr1e-4_wd1e-3_bs512____rerun_finetune_after_self_train_epoch183_iter0290000.pt.12.fp16_masking.onnx \
   --num-workers 32 \
-  --val-batch-size 2 \
+  --val-batch-size 256 \
   --checkpoint best_checkpoints/JasperNetBig_NovoGrad_lr1e-4_wd1e-3_bs512____rerun_finetune_after_self_train_epoch183_iter0290000.pt \
-  --val-data-path domain_set/transcripts/16082020/valset_16082020.0.csv.json
+  --val-data-path domain_set/transcripts/16082020/valset_16082020.0.csv.json domain_set/transcripts/16082020/valset_16082020.1.csv.json
